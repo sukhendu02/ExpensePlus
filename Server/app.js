b7d88724e917a957d37953baa8d26676cca021e1
Sukhendu <sukhendumandal02@gmail.com>
2026-06-05 17:30:09 +0530

    fix:update PATCH method in cors

--- Server/app.js
+++ Server/app.js
@@ -17,7 +17,7 @@ app.use(helmet());
 // CORS
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   })
 );
 
